61648b8dc2e411800a0b82724c4b7054ac72c7e6
Hagbard235 <github@andreschmidt.de>
2017-06-07 14:49:24 +0200

    .

--- GardenaSileno/module.php
+++ GardenaSileno/module.php
@@ -110,6 +110,30 @@
 				
 
         }
+		public function CheckUsernameUndPassword() {
+            // Selbsterstellter Code
+			    $username = $this->ReadPropertyString("Username");
+				$password = $this->ReadPropertyString("Password");
+			
+				
+				$data = array(
+            "sessions" => array(
+                "email" => "$username", "password" => "$password")
+            );
+        $data_string = json_encode($data);
+        $ch = curl_init(self::LOGINURL);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type:application/json',
+            'Content-Length: ' . strlen($data_string))
+        );
+        $result = curl_exec($ch);
+			echo ($result);	
+			   
+		}
+		
 		public function GeraeteInfosAktualisieren() {
             // Selbsterstellter Code
 			    $username = $this->ReadPropertyString("Username");
