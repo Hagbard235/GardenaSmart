045288015f66d9f577b40eb72a3f3dcdf19c54e7
Hagbard235 <github@andreschmidt.de>
2017-06-13 16:32:50 +0200

    .

--- GardenaSileno/module.php
+++ GardenaSileno/module.php
@@ -21,7 +21,7 @@
             $this->RegisterPropertyString("Password", "Password"); 
 			$this->RegisterPropertyInteger("Interval",5); 
 			$this->RegisterPropertyInteger("IntervalB",5); 
-			$this->RegisterPropertyInteger("IntervalB",0); 
+			$this->RegisterPropertyInteger("IntervalF",0); 
 			
 			//Variablenprofil anlegen ($name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon)
 		$profilename = "GAR.Befehle";
